c4170463257f31948391e965222bd8ae50a2837b
Luis Gabriel <luis.gabo.rex@gmail.com>
2025-09-09 19:30:41 -0600

    feat: get teams table and get every team row

--- src/HttpClient.java
+++ src/HttpClient.java
@@ -6,15 +6,32 @@ import org.jsoup.select.Elements;
 import java.io.IOException;
 
 public class HttpClient {
+    Document html;
+    Elements teamsTable;
 
     public void connectToPage() {
         try {
-            Document html = Jsoup.connect("https://www.baseball-reference.com/teams/").get();
-            Elements teamsTable = html.select("#teams_active caption");
+            html = Jsoup.connect("https://www.baseball-reference.com/teams/").get();
         } catch (IOException e) {
             System.err.println("Error: " + e.getMessage());
         }
     }
+
+    public void getTable() {
+        teamsTable = html.select("#teams_active");
+        Elements teamsRow = teamsTable.select("tbody tr:not(.partial_table)");
+        for (Element teamRow : teamsRow) {
+            System.out.println(teamRow);
+        }
+    }
+
+    public void getTeamsRow() {
+        Elements teamsRow = teamsTable.select("tbody tr:not(.partial_table)");
+        for (Element teamRow : teamsRow) {
+            System.out.println(teamRow);
+        }
+    }
+
 }
 
 
